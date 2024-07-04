553a1372896e19ae83771432ec6cede7d983bec1
bobo <wing0453@gmail.com>
2024-07-05 01:15:56 +1000

    outlined the database structure

--- backend/hosting/src/main.ts
+++ backend/hosting/src/main.ts
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getDatabase, ref, set } from "firebase/database";
+import { getDatabase, ref, set, push, update, on } from "firebase/database";
 
 
 const firebaseConfig = {
@@ -15,9 +15,9 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-function writeUserData(userId: string, name: string, email: string, imageUrl: string) {
-  const db = getDatabase(app);
+const db = getDatabase(app);
 
+function writeUserData(userId: string, name: string, email: string, imageUrl: string) {
   // where in database you read and write data
   // ref(db, path)
   const reference = ref(db, 'users/' + userId);
@@ -29,6 +29,81 @@ function writeUserData(userId: string, name: string, email: string, imageUrl: st
     profile_picture: imageUrl
   });
 }
+
+function writeActivity(activityId: string, activityName: string, options: string, questions: any, questionsNo: number, badges: any, status: boolean) {
+  const reference = ref(db, 'activity/' + activityId);
+
+  set(reference, {
+    activityName: activityName,
+    options: options,
+    questionsNo: questionsNo,
+    badges: badges,
+    status: status
+  })
+  questions.forEach((element: { index: number; question: string; answer: string; options: any; }) => {
+    writeQuestion(activityId, element.index, element.question, element.answer, element.options)
+  })
+
+  //maybe no need manually input question n options one by one
+
+  function writeQuestion(id:string, order: number, question: string, answer: string, options: any) {
+    const reference = ref(db, 'activity/' + id + 'question/' + order);
+  
+    update(reference, {
+      question: question,
+      answer: answer,
+    })
+  
+    options.forEach((element: string) => {
+      writeOption(id, order, element)
+    })
+  
+      function writeOption(id: string, order: number, option: string) {
+        const reference  = ref(db, 'activity/' + id + 'question/' + order + 'options/');
+      
+        update(reference, {
+          choice: option
+        })
+      }
+  }
+  
+}
+
+function writeBadge(badgeId: string, name: string, colour: string, conditions: number) {
+  const reference = ref(db, 'badges/' + badgeId);
+
+  set(reference, {
+    name: name,
+    colour: colour,
+    conditions: conditions
+  })
+}
+
+function giveBadge(userId: string, badgeId: string) {
+  const reference = ref(db, 'users/' + userId);
+
+  push(reference, badgeId);
+}
+
+function getAllActivities() { 
+  const reference = ref(db, 'activity');
+  var allAct;
+  on(reference, 'value', function(snapshot: any) {
+    allAct = snapshot.val()
+  })
+  return allAct
+}
+
+function getActivity(activityId: string) {
+  const reference = ref(db, 'activity' + activityId);
+  var activity;
+  on(reference, 'value', function(snapshot: any) {
+    activity = snapshot.val()
+  })
+  return activity
+}
+
+
 
 writeUserData("andreawu", "awu", "myemail@me.com", "myimageurl");
 
