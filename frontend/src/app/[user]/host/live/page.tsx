f0cf71825aec22157b9f40f0a548c293c399fd09
bevans <81346899+bevans-o@users.noreply.github.com>
2024-07-05 13:46:51 +1000

    setup host view

--- frontend/src/app/[user]/host/live/page.tsx
+++ frontend/src/app/[user]/host/live/page.tsx
@@ -1,7 +1,16 @@
+import LiveFooter from "@/app/components/layout/LiveFooter";
+import LiveHeader from "@/app/components/layout/LiveHeader";
+import StreamView from "@/app/components/layout/StreamView";
 import React from "react";
 
 function Page() {
-  return <div>Page</div>;
+  return (
+    <StreamView>
+      <LiveHeader className="absolute top-0" />
+
+      <LiveFooter className="absolute bottom-0" />
+    </StreamView>
+  );
 }
 
 export default Page;
--- frontend/src/app/components/layout/StreamView.tsx
+++ frontend/src/app/components/layout/StreamView.tsx
@@ -5,7 +5,7 @@ function StreamView({ children }: { children?: React.ReactNode }) {
     <div className="relative w-full h-full">
       <div className="absolute w-full h-full inset-0 z-10">{children}</div>
       <div className="absolute w-full h-full bg-gradient-to-t from-neutral-900 to-neutral-800">
-        <img src={"/background.png"} className="brightness-90" />
+        <img src={"/background.png"} className="brightness-90 w-full h-full object-cover" />
       </div>
     </div>
   );
