e18678418171e47f69d331ad36dd1022d814e301
bevans <81346899+bevans-o@users.noreply.github.com>
2024-07-05 12:13:52 +1000

    add mock functions

--- frontend/src/app/lib/badge.ts
+++ frontend/src/app/lib/badge.ts
@@ -1,1 +1,29 @@
-export type Badge = {};
+export type Colour = "red" | "rose" | "blue" | "green" | "yellow" | "teal" | "purple";
+export type Icon = "star" | "heart" | "flower";
+
+export type Badge = {
+  name: string;
+  colour: Colour;
+  icon: Icon;
+};
+
+// MOCK: post
+export function postBadge(badge: Badge) {
+  console.log(`POST Badge: ${badge.name}`);
+  console.log(badge);
+}
+
+// MOCK: get all
+export function getBadges(user: string | undefined): Badge[] {
+  if (!user) return [];
+  return [];
+}
+
+// MOCK: get one
+export function getBadge(id: string): Badge {
+  return {
+    name: "Badge",
+    colour: "red",
+    icon: "heart",
+  };
+}
--- frontend/src/app/lib/stream.ts
+++ frontend/src/app/lib/stream.ts
@@ -1,1 +1,15 @@
-export type Stream = {};
+import { Activity } from "./activity";
+
+export type Stream = {
+  user: string;
+  activity?: Activity;
+  currentQuestion: number;
+
+  // some way of tracking answers
+};
+
+// hook to subscribe to current question
+export function useCurrentQuestion(streamId: string) {}
+
+// hook to get leaderboard
+export function useLeaderboard(streamId: string) {}
