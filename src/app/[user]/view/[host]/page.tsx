c3b9afbf39712460859afc68b86b251d25dbc54f
bobo <wing0453@gmail.com>
2024-07-06 18:37:17 +1000

    layout

--- src/app/[user]/view/[host]/page.tsx
+++ src/app/[user]/view/[host]/page.tsx
@@ -1,7 +1,43 @@
 import React from "react";
 
+import ActivityModal from "@/app/components/layout/ActivityModal";
+import PreLiveFooter from "@/app/components/layout/PreLiveFooter";
+import PreLiveHeader from "@/app/components/layout/PreLiveHeader";
+import StreamView from "@/app/components/layout/StreamView";
+import { postStream, Stream } from "@/app/lib/stream";
+import { useRouter, useSearchParams } from "next/navigation";
+import React, { useState } from "react";
+
+
 function Page() {
-  return <div>Page</div>;
+  return (<StreamView>
+  <PreLiveHeader
+    className="absolute top-0"
+    title={`${params.user}'s Stream`}
+    activity={stream.activity}
+    badge={stream.badge}
+    onClick={() => setModalOpen(true)}
+  />
+  <PreLiveFooter
+    className="absolute bottom-0"
+    onSelectActivities={() => setModalOpen(true)}
+    onStart={async () => {
+      await postStream(stream);
+      router.push(`/${params.user}/host/live`);
+    }}
+  />
+
+  {modalOpen && (
+    <ActivityModal
+      user={params.user}
+      onSelect={(a, b) => {
+        setStream({ ...stream, activity: a, badge: b });
+        setModalOpen(false);
+      }}
+      onClose={() => setModalOpen(false)}
+    />
+  )}
+</StreamView>)
 }
 
 export default Page;
